37f0f774111bb91714c185bf8efe5a8a4f7059da
Sonya Duke <sonya.j.duke@gmail.com>
2020-05-02 15:07:58 +1000

    Feature: user can see list of cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+drinks_serialised = open(url).read
+drinks = JSON.parse(drinks_serialised)
+
+puts "Starting seed"
+Ingredient.destroy_all
+
+drinks["drinks"].each do |ingredient|
+  ingred = Ingredient.create(name: ingredient["strIngredient1"])
+  puts ingred.name
+end
+
+puts "Finished seed"
